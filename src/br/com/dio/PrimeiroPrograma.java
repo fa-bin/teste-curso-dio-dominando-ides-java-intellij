b01101d42e57dbe7355d7be56f8d7c71ece4ed16
Fabio Mendonca <f_mendonca@outlook.com.br>
2021-10-29 20:01:30 -0300

    first commit

--- src/br/com/dio/PrimeiroPrograma.java
+++ src/br/com/dio/PrimeiroPrograma.java
@@ -2,11 +2,14 @@ package br.com.dio;
 
 import br.com.dio.model.Gato;
 
+import java.util.Objects;
+
 public class PrimeiroPrograma {
     public static void main(String[] args) {
         Gato gato = new Gato();
-
+        Livro livro = new Livro("O problema dos 3 corpos", 300);
         System.out.println(gato);
+        System.out.println(livro);
 
 
         /*int a = 5;
@@ -14,5 +17,40 @@ public class PrimeiroPrograma {
         System.out.println("Hello Fa.bin " + (a+b));*/
 
     }
-
 }
+
+    class Livro{
+        private String nome;
+        private Integer numPaginas;
+
+        public Livro(String nome, Integer numPaginas) {
+            this.nome = nome;
+            this.numPaginas = numPaginas;
+        }
+
+        public String getNome() {
+            return nome;
+        }
+
+        public void setNome(String nome) {
+            this.nome = nome;
+        }
+
+        public Integer getNumPaginas() {
+            return numPaginas;
+        }
+
+        public void setNumPaginas(Integer numPaginas) {
+            this.numPaginas = numPaginas;
+        }
+
+        @Override
+        public String toString() {
+            return "Livro{" +
+                    "nome='" + nome + '\'' +
+                    ", numPaginas=" + numPaginas +
+                    '}';
+        }
+}
+
+
